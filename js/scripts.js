d6c976580ed08db8f917550599bb5c812ac1a333
farisfs73x <wanmohammadfaris@student.usm.my>
2021-12-15 03:02:56 +0800

    added progressbar & update meta tags

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,13 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+// ------------------Prgressbar------------------
+
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function(){
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+}
