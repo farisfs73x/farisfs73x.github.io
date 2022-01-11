49c745cc719ed6588cd147487f6d0412e05577a7
farisfs73x <wanmohammadfaris@student.usm.my>
2022-01-11 15:47:07 +0800

    Added quotes

--- js/scripts.js
+++ js/scripts.js
@@ -64,17 +64,31 @@ window.onscroll = function(){
 var textWrapper = document.querySelector('.ml9 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
-  .add({
+anime.timeline({
+    loop: true
+}).add({
     targets: '.ml9 .letter',
     scale: [0, 1],
     duration: 1500,
     elasticity: 600,
-    delay: (el, i) => 45 * (i+1)
-  }).add({
+    delay: (el, i) => 45 * (i + 1)
+}).add({
     targets: '.ml9',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
     delay: 1000
-  });
+});
+
+
+// ------------------Quote------------------
+
+$.getJSON('https://programming-quotes-api.herokuapp.com/Quotes/random', function(data) {
+
+    var quote = `${data.en}`
+    var author = `${data.author}`
+
+    $("#quote").html(quote);
+    $("#quote_author").html(author);
+
+});
